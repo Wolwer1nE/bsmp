239926363bdea4301800274e14f0ae4f9b60a758
Pavel Oganesyan <oganesyan@hey.com>
2025-11-25 23:34:39 +0800

    Fix matlab mult script

--- matlab/multiplication.m
+++ matlab/multiplication.m
@@ -1,6 +1,6 @@
 clear, clc; format long 
 data = load('../data/100000.txt');
-n = 1;
+n_runs = 10;
 i = data(:,1) + 1;
 j = data(:,2) + 1;
 values = data(:,3);
@@ -9,8 +9,8 @@ n = max([max(i), max(j)]);
 A = sparse(i, j, values, n, n);
 b = load('../data/rhs_100000.txt');
 tic
-for k = 1:n
+for k = 1:n_runs
     x = A*b;
 end
 t = toc;
-t/n*1
+fprintf("Average time: %f seconds\n", t/n_runs)
